002d4c2676e3124f62022b56934826841d6bdfd4
Solirize <ximikstalker@gmail.com>
2019-11-16 19:47:03 +0300

    add Die

---
+++ Assets/Scripts/Hero/HeroDeath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class HeroDeath : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 10)
+            Die();
+    }
+    public void Die()
+        {
+            //play dying anim;
+            //Destroy(this.gameObject);
+            SceneManager.LoadScene("SampleScene");//Change name of scene???
+        }
+}
