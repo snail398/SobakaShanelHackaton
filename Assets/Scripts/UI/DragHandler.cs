2bf593a7ba5333a3e02af4d3e46263bcabe559a1
snail398 <lololo398@yandex.ru>
2019-11-16 20:34:00 +0300

    fix adding pit

--- Assets/Scripts/UI/DragHandler.cs
+++ Assets/Scripts/UI/DragHandler.cs
@@ -40,15 +40,18 @@ namespace UI
 
         public void OnEndDrag(PointerEventData eventData)
         {
+            Vector3 her = Camera.main.ScreenToWorldPoint(eventData.position);
+            Vector3 instPos = new Vector3(her.x, her.y, 0);
             if (_prefab is PitView)
             {
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 var hit = Physics2D.Raycast(ray.origin, ray.direction, 100, ~LayerMask.NameToLayer("Ground"));
                 if (hit)
+                {
                     Destroy(hit.collider.gameObject);
+                    instPos = hit.collider.transform.position;
+                }
             }
-            Vector3 her = Camera.main.ScreenToWorldPoint(eventData.position);
-            Vector3 instPos = new Vector3(her.x, her.y, 0);
             Instantiate(_prefab, instPos, Quaternion.identity);
             if (transform.parent == startParent)
             {
