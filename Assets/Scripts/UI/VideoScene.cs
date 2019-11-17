7c7ceda098eedf8d51ed0b93130ab26eb98671e8
snail398 <lololo398@yandex.ru>
2019-11-17 18:28:44 +0300

    finish video scene

--- Assets/Scripts/UI/VideoScene.cs
+++ Assets/Scripts/UI/VideoScene.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+using UniRx;
 
 public class VideoScene : MonoBehaviour
 {
+    [SerializeField] private VideoPlayer _player;
+
+
+    private void Awake()
+    {
+        Observable.Timer(System.TimeSpan.FromSeconds(_player.length)).Subscribe(_=> MoveToNextScene()).AddTo(this);
+    }
+
     public void MoveToNextScene()
     {
         SceneManager.LoadScene("SampleScene");
     }
+
+
 }
