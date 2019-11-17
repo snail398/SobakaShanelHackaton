f9186810bd4bb80c33a1b97198e5700a829a6565
Solirize <ximikstalker@gmail.com>
2019-11-17 18:46:30 +0300

    fix img

--- Assets/SceneSelect.cs
+++ Assets/SceneSelect.cs
@@ -1,18 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneSelect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void SelectScene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("SampleScene");
     }
 }
