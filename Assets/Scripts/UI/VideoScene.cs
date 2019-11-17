fd19b4d069fdca07a0ac01b4c9cec2098e5d6a0c
snail398 <lololo398@yandex.ru>
2019-11-17 18:19:53 +0300

    add video Scene

---
+++ Assets/Scripts/UI/VideoScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoScene : MonoBehaviour
+{
+    public void MoveToNextScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
